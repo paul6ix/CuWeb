91de52ac22ebd2f5d1c909b402c57ee5a77800ac
paul6ix <okporp@gmail.com>
2015-07-10 20:22:21 +0100

    Initial commit

---
+++ app/src/main/java/com/sixtech/paulchidi/cuweb/MainActivity.java
@@ -0,0 +1,140 @@
+package com.sixtech.paulchidi.cuweb;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends ListActivity {
+    protected ImageView ivSplash;
+    protected TextView tvSplash;
+
+
+
+
+    protected  String pWebsites[] = {
+            "DataCenter",
+            "Student Portal",
+            "Moodle",
+            "Covenant university",
+            "Chaplaincy",
+            "Ice 2018",
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvSplash = (TextView) findViewById(R.id.textView);
+        ivSplash = (ImageView) findViewById(R.id.imageView);
+        ivSplash.setVisibility(View.VISIBLE);
+
+        final Animation splashText =  AnimationUtils.loadAnimation(MainActivity.this,R.anim.splashtext);
+        tvSplash.startAnimation(splashText);
+
+
+
+        ivSplash.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ivSplash.setVisibility(View.INVISIBLE);
+                tvSplash.setVisibility(View.INVISIBLE);
+
+
+
+            }
+        });
+
+
+        ArrayAdapter<String> aaCuweb = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1, pWebsites );
+        setListAdapter(aaCuweb);
+
+
+
+
+    }
+
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        if (position ==1){
+            Intent intent = new Intent(this, ViewWebsite.class);
+            intent.setData(Uri.parse("http://portal.covenantuniversity.edu.ng/"));
+            startActivity(intent);
+
+
+        }
+       else if (position == 0){
+            Intent intent = new Intent(this, ViewWebsite.class);
+            intent.setData(Uri.parse("Datacenter.cu"));
+            startActivity(intent);
+
+        }
+        else if (position == 2){
+            Intent intent = new Intent(this, ViewWebsite.class);
+            intent.setData(Uri.parse("http://moodle2.covenantuniversity.edu.ng/"));
+            startActivity(intent);
+
+        }
+        else if (position == 3){
+            Intent intent = new Intent(this, ViewWebsite.class);
+            intent.setData(Uri.parse("http://covenantuniversity.edu.ng/"));
+            startActivity(intent);
+
+        }
+        else if (position == 4){
+            Intent intent = new Intent(this, ViewWebsite.class);
+            intent.setData(Uri.parse("http://chaplaincy.covenantuniversity.edu.ng/"));
+            startActivity(intent);
+
+        }
+        else if (position == 5){
+            Intent intent = new Intent(this, ViewWebsite.class);
+            intent.setData(Uri.parse("http://ice2018.com/"));
+            startActivity(intent);
+
+        }
+
+            super.onListItemClick(l, v, position, id);
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/sixtech/paulchidi/cuweb/ViewWebsite.java
@@ -0,0 +1,76 @@
+package com.sixtech.paulchidi.cuweb;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+
+public class ViewWebsite extends Activity {
+    protected ProgressBar pbWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_website);
+        Intent iWebsite = getIntent();
+        Uri uWeburl = iWebsite.getData();
+        WebView wvWebsite = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = wvWebsite.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSaveFormData(true);
+        wvWebsite.setHorizontalScrollBarEnabled(true);
+        wvWebsite.setVerticalScrollBarEnabled(true);
+
+        wvWebsite.setWebViewClient(new mywebview());
+
+        wvWebsite.loadUrl(uWeburl.toString());
+
+
+
+        ImageButton ibBack = (ImageButton) findViewById(R.id.BtnBack);
+        ibBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ViewWebsite.this, MainActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+
+    private class mywebview extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            pbWeb = (ProgressBar) findViewById(R.id.progressBar);
+            pbWeb.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            pbWeb.setVisibility(View.GONE);
+
+        }
+        
+    }
+}
+
